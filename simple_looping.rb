a945fbd3f29a1aa02b9e91f15d58dae69c8495c6
Scarlett Perry <si.perry15@gmail.com>
2018-06-30 22:28:07 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,6 +9,7 @@ def loop_iterator(number_of_times)
       break 
   end
 end 
+end 
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
