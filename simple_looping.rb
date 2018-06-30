43ece0d18d830208f8784e9340f951e36af6bcaf
Scarlett Perry <si.perry15@gmail.com>
2018-06-30 22:36:23 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -27,7 +27,7 @@ end
 
 def while_iterator(number_of_times)
   counter = 0 
-  while counter <= number_of_times
+  while counter < number_of_times
     counter += 1
     puts "Welcome to Flatiron School's Web Development Course!"
   end 
