92788085563c2ccb34a35b2c907e38dcdecfe561
Scarlett Perry <si.perry15@gmail.com>
2018-06-30 22:38:08 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,7 +35,7 @@ end
 
 def until_iterator(number_of_times)
   counter = 0 
-  until counter > number_of_times
+  until counter == number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1
   end 
